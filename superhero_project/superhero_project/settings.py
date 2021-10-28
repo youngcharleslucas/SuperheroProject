87b8c8d6850fcb704a62ee6dce0969d49f235070
Your Name <charles.luas.young@gmail.com>
2021-10-28 16:03:48 -0600

    Migrate manage.py

--- superhero_project/superhero_project/settings.py
+++ superhero_project/superhero_project/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'superhero_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+		'NAME': 'superhero_database',
+		'USER': 'root',
+		'PASSWORD': 'QAZwsx12!@',
+		'HOST': '127.0.0.1',
+		'PORT': '3306',
+		'OPTIONS': {
+			'autocommit': True
+        }
     }
 }
 
