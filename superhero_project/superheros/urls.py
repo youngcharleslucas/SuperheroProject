0d84ca0a0bdf8009a1f25fcb59d80ea70f7e3552
Your Name <charles.luas.young@gmail.com>
2021-10-29 15:03:02 -0600

    Create Create character html

--- superhero_project/superheros/urls.py
+++ superhero_project/superheros/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'superheros'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:hero_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create')
 ]
--- superhero_project/superheros/views.py
+++ superhero_project/superheros/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from .models import Superhero
 
@@ -10,3 +11,24 @@ def index(request):
         'all_heros' : all_heros
     }
     return render(request, 'superheros/index.html', context )
+
+def detail(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    return render(request, 'superheros/detail.html')
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego =request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheros:index'))
+
+    else:
+        return render(request, 'superheros/create.html')
