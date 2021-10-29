0367959da1ddd90df200e742b5fd4744952817a8
Your Name <charles.luas.young@gmail.com>
2021-10-29 13:06:40 -0600

    Add debug fix ExecutionPolicy

--- superhero_project/superheros/views.py
+++ superhero_project/superheros/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Superhero
 
 # Create your views here.
+
+def index(request):
+    all_heros = Superhero.objects.all()
+    context = {
+        'all_heros' : all_heros
+    }
+    return render(request, 'superheros/index.html', context )
