cf98a8c7e5ddcf7c5ed3a823dd46d0825bb1acb5
Your Name <charles.luas.young@gmail.com>
2021-10-29 08:29:35 -0600

    Create App class Superhero

---
+++ superhero_project/superheros/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Superhero(models.Model):
+    name = models.CharField (max_length=50)
+    alter_ego = models.CharField (max_length=50)
+    primary_ability = models.CharField (max_length=50)
+    secondary_ability = models.CharField (max_length=50)
+    catch_phrase = models.CharField (max_length=50)
+
+    def __str__(self):
+        return self.name
